41e838de22a0a2a9895a65100dcc43c8d2b5c5d8
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2025-08-05 17:25:28 +0300

    .nav-button swaps page

--- src/nav.js
+++ src/nav.js
@@ -3,11 +3,17 @@ import { capitalize } from "./utils";
 export default function buildNav(container, pages) {
   const buttons = pages.map((p) => buildButton(p));
   container.append(...buttons);
+
+  function buildButton(page) {
+    const node = document.createElement("button");
+    node.textContent = capitalize(page.name);
+    node.dataset.page = page.name;
+    node.addEventListener("click", () => swapPage(page));
+    return node;
+  }
 }
 
-function buildButton(page) {
-  const node = document.createElement("button");
-  node.textContent = capitalize(page.name);
-  node.dataset.page = page.name;
-  return node;
+function swapPage(page) {
+  const node = document.getElementById("content");
+  node.innerHTML = page.html;
 }
